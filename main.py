87ab28bb4504aaac5691cbfc90a7f7b402cf4d80
Justin Welenofsky <welenofsky@gmail.com>
2017-07-12 12:57:00 -0700

    Fix clipboard to upper for Mac

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
-try:
-    # Python2
-    import Tkinter as tk
-except ImportError:
-    # Python3
-    import tkinter as tk
+import pyperclip
 
-root = tk.Tk()
-# keep the window from showing
-root.withdraw()
+clipboard_contents = pyperclip.paste()
 
-# text from clipboard
-clip_text = root.clipboard_get()
-clip_text = clip_text.upper()
-
-# clear the clipboard
-root.clipboard_clear()
-
-# put uppercase text into clipboard
-root.clipboard_append(clip_text)
+if len(clipboard_contents):
+    pyperclip.copy(clipboard_contents.upper())
